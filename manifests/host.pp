2680062eb20c8a3008990203a3967d897d8abcb1
Jorgen <jur.public@xs4all.nl>
2017-10-06 10:58:53 +0200

    Added Nagios XI API call to delete a certain host
    Added Nagios XI API call to delete a certain host if ensure = absent

--- manifests/host.pp
+++ manifests/host.pp
@@ -16,7 +16,7 @@ define nagiosxi::host (
     }
   } else {
     exec { "nagiosxi_host_${title}":
-      command => "curl -XPOST \"${nagiosxi::nagios_url}/nagiosxi/api/v1/config/host?apikey=${nagiosxi::nagios_apikey}\" -d \"host_name=${title}&address=${address}&alias=${nagios_alias}&use=${nagios_host_template}&force=1&applyconfig=1\"",
+      command => "curl -XDELETE \"${nagiosxi::nagios_url}/nagiosxi/api/v1/config/host?apikey=${nagiosxi::nagios_apikey}&host_name=${title}&applyconfig=1\"",
       path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
       unless  => "curl -s -XGET \"${nagiosxi::nagios_url}/nagiosxi/api/v1/objects/host?apikey=${nagiosxi::nagios_apikey}&host_name=${title}\" | grep '\"recordcount\":\"0\"'",
     }
